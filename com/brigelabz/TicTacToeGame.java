97263e529c6be4af1069151f4eedc30800555204
ShitalKarad <shitalkarad24@gmail.com>
2022-11-25 19:58:41 +0530

    Added : uc2 allow to player choice X or O

--- com/brigelabz/TicTacToeGame.java
+++ com/brigelabz/TicTacToeGame.java
@@ -1,9 +1,12 @@
 package com.brigelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome in tic tac toy ");
        createEmptyBoard();
+       allowPlayer();
     }
     public static void createEmptyBoard(){
         char[] sizeOfBoard = new char[10];
@@ -13,9 +16,21 @@ public class TicTacToeGame {
             if (i % 3 == 0) {
                 System.out.println("|");
             }
-
         }
-       
+
+    }
+    public static void allowPlayer(){
+        System.out.println("Enter your choice if wont 'X' then press 1 & if you wont 'O' then press 2");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        if ( choice == 1){
+            System.out.println(" player => x");
+            System.out.println("Computer => O");
+        } else if (choice == 2) {
+            System.out.println(" player => O");
+            System.out.println("Computer => X");
+        }
+
     }
 
 }
